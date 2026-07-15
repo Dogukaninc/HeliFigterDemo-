ba20ae9566851085e028d8b7f47b552e220fc744
Doğukan <d_incirbolen@hotmail.com>
2026-07-15 16:07:23 +0300

    Update : GOAP agent implementation

---
+++ Assets/_main/Scripts/Bot Ai Mind/GoapFactory.cs
@@ -0,0 +1,17 @@
+﻿using DependencyInjection;
+using UnityEngine;
+using UnityServiceLocator;
+
+public class GoapFactory : MonoBehaviour, IDependencyProvider
+{
+    void Awake()
+    {
+        ServiceLocator.Global.Register(this);
+    }
+
+    [Provide] public GoapFactory ProvideFactory() => this;
+    public IGoapPlanner CreatePlanner()
+    {
+        return new GoapPlanner();
+    }
+}
